f542e3322729153203bc9281a74ea611cc694239
huangz826 <huang.zix@northeastern.edu>
2023-12-07 14:01:29 -0800

    fix bugs

--- lab5.js
+++ lab5.js
@@ -14,7 +14,7 @@ const todos = [
 ];
 
 const Lab5 = (app) => {
-  app.put("/a5/todos/:id", (req, res) => {
+  app.put("/api/a5/todos/:id", (req, res) => {
     const { id } = req.params;
     const todo = todos.find((t) => t.id === parseInt(id));
     todo.title = req.body.title;
@@ -23,19 +23,19 @@ const Lab5 = (app) => {
     todo.completed = req.body.completed;
     res.sendStatus(200);
   });
-  app.delete("/a5/todos/:id", (req, res) => {
+  app.delete("/api/a5/todos/:id", (req, res) => {
     const { id } = req.params;
     const todo = todos.find((t) => t.id === parseInt(id));
     todos.splice(todos.indexOf(todo), 1);
     res.sendStatus(200);
   });
-  app.get("/a5/todos/:id/delete", (req, res) => {
+  app.get("/api/a5/todos/:id/delete", (req, res) => {
     const { id } = req.params;
     const todo = todos.find((t) => t.id === parseInt(id));
     todos.splice(todos.indexOf(todo), 1);
     res.json(todos);
   });
-  app.post("/a5/todos", (req, res) => {
+  app.post("/api/a5/todos", (req, res) => {
     const newTodo = {
       ...req.body,
       id: new Date().getTime(),
@@ -43,7 +43,7 @@ const Lab5 = (app) => {
     todos.push(newTodo);
     res.json(newTodo);
   });
-  app.get("/a5/todos/create", (req, res) => {
+  app.get("/api/a5/todos/create", (req, res) => {
     const newTodo = {
       id: new Date().getTime(),
       title: "New Task",
@@ -52,18 +52,18 @@ const Lab5 = (app) => {
     todos.push(newTodo);
     res.json(todos);
   });
-  app.get("/a5/todos/:id", (req, res) => {
+  app.get("/api/a5/todos/:id", (req, res) => {
     const { id } = req.params;
     const todo = todos.find((t) => t.id === parseInt(id));
     res.json(todo);
   });
-  app.get("/a5/todos/:id/title/:title", (req, res) => {
+  app.get("/api/a5/todos/:id/title/:title", (req, res) => {
     const { id, title } = req.params;
     const todo = todos.find((t) => t.id === parseInt(id));
     todo.title = title;
     res.json(todos);
   });
-  app.get("/a5/todos", (req, res) => {
+  app.get("/api/a5/todos", (req, res) => {
     const { completed } = req.query;
     if (completed !== undefined) {
       const status = completed === "true";
@@ -73,41 +73,41 @@ const Lab5 = (app) => {
     }
     res.json(todos);
   });
-  app.get("/a5/assignment/completed/:newCompleted", (req, res) => {
+  app.get("/api/a5/assignment/completed/:newCompleted", (req, res) => {
     const { newCompleted } = req.params;
     assignment.completed = newCompleted;
     res.json(assignment);
   });
-  app.get("/a5/assignment/score/:newScore", (req, res) => {
+  app.get("/api/a5/assignment/score/:newScore", (req, res) => {
     const { newScore } = req.params;
     assignment.score = newScore;
     res.json(assignment);
   });
-  app.get("/a5/assignment/title/:newTitle", (req, res) => {
+  app.get("/api/a5/assignment/title/:newTitle", (req, res) => {
     const { newTitle } = req.params;
     assignment.title = newTitle;
     res.json(assignment);
   });
-  app.get("/a5/assignment", (req, res) => {
+  app.get("/api/a5/assignment", (req, res) => {
     res.json(assignment);
   });
-  app.get("/a5/assignment/title", (req, res) => {
+  app.get("/api/a5/assignment/title", (req, res) => {
     res.json(assignment.title);
   });
-  app.get("/a5/welcome", (req, res) => {
+  app.get("/api/a5/welcome", (req, res) => {
     res.send("Welcome to Assignment 5");
   });
-  app.get("/a5/add/:a/:b", (req, res) => {
+  app.get("/api/a5/add/:a/:b", (req, res) => {
     const { a, b } = req.params;
     const sum = parseInt(a) + parseInt(b);
     res.send(sum.toString());
   });
-  app.get("/a5/subtract/:a/:b", (req, res) => {
+  app.get("/api/a5/subtract/:a/:b", (req, res) => {
     const { a, b } = req.params;
     const sum = parseInt(a) - parseInt(b);
     res.send(sum.toString());
   });
-  app.get("/a5/calculator", (req, res) => {
+  app.get("/api/a5/calculator", (req, res) => {
     const { a, b, operation } = req.query;
     let result = 0;
     switch (operation) {
